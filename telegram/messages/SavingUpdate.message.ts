f0e0bf3474cca82351b53d76528d36d314d8ffd8
bernd2022 <bernd.nospam.2018@t-online.de>
2025-04-10 13:14:35 +0200

    Add refcode to telegram savings message

--- telegram/messages/SavingUpdate.message.ts
+++ telegram/messages/SavingUpdate.message.ts
@@ -4,12 +4,15 @@ import { formatCurrency } from 'utils/format';
 import { formatUnits } from 'viem';
 
 export function SavingUpdateMessage(saving: SavingsSavedQuery): string[] {
+	const usedRef = saving.refCode ? `🪢 used Ref: ${saving.refCode}` : '';
+
 	const message = `
 *New dEURO Savings!*
 
 🔏 Savings Amount: *${formatCurrency(formatUnits(BigInt(saving.amount), 18))}*
 🧲 ${formatCurrency(formatUnits(BigInt(saving.rate), 4))}% APR
 👤 [Saver](https://etherscan.io/address/${saving.account}) / [TX](https://etherscan.io/tx/${saving.txHash})
+${usedRef}
 `;
 
 	const image = `${CONFIG.telegramImagesDir}/Savings_Telegram.mp4`;
