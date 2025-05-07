cd75f0c4534d06fc4c5f43f504f0e478c08da939
bernd2022 <bernd.nospam.2018@t-online.de>
2025-05-07 19:36:52 +0200

    added frontend code to trades messages

--- trades/trade.service.ts
+++ trades/trade.service.ts
@@ -21,6 +21,7 @@ export class TradesService {
                             trader
                             amount
                             shares
+                            frontendCode
                         }
                     }
                 }
